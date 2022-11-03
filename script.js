f4f98ccef9f0cbb1e3cdd5011083679aa7729b0c
mrodz <79176075+mrodz@users.noreply.github.com>
2022-11-02 21:20:29 -0700

    refactoring for yanga

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-let character = document.getElementById("character"); 
+let character = document.getElementById("character");
 let block = document.getElementById("block");
 let counter = 0;
 
-function jump(){
-    if(character.classList == "animate"){return}
+/**
+ * Make the player jump.
+ */
+function jump() {
+    if (character.classList == "animate") { return }
     character.classList.add("animate");
-    setTimeout(function(){
+    setTimeout(function () {
         character.classList.remove("animate");
-    }, 300);   
+    }, 300);
 }
 
-let checkDeath = setInterval(function() {
+/**
+ * Set the bounds for the box's collision range.
+ */
+const COLLISION_BOX = {
+    right: 20,
+    left: -20
+}
+
+/**
+ * Checks whether the block collides, by the boundaries set in {@link COLLISION_BOX}.
+ * @param {number} blockCoord 
+ * @returns whether the player collides.
+ */
+function collides(blockCoord) {
+    return blockCoord < COLLISION_BOX.right && blockCoord > COLLISION_BOX.left
+}
+
+/**
+ * Checks if a player is jumping.
+ * @param {number} characterTop 
+ * @returns whether the player is jumping.
+ */
+function isJumping(characterTop) {
+    return characterTop < 130
+}
+
+let checkDeath = setInterval(function () {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft< 20 && blockLeft> -20 && characterTop>= 130) {
+
+    if (collides(blockLeft) && !isJumping(characterTop)) {
         block.style.animation = "none";
-        alert("Game Over :( Score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";  
-    } else{
+        alert("Game Over :( Score: " + Math.floor(counter / 100));
+        counter = 0;
+        block.style.animation = "block 1s infinite linear";
+    } else {
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
     }
 }, 10);
